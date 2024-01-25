26cdcd8871c189eab5889c83d14f6c9d1be7cfdc
Uriel Ofir <urielofir@gmail.com>
2024-01-25 17:10:12 +0200

    basic code of extension

---
+++ extensions/checkout-ui/src/Checkout.tsx
@@ -0,0 +1,26 @@
+import {
+  Button,
+  Banner,
+  useApi,
+  useTranslate,
+  reactExtension,
+} from '@shopify/ui-extensions-react/checkout';
+
+export default reactExtension(
+  'purchase.checkout.block.render',
+  () => <Extension />,
+);
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension  } = useApi();
+
+
+  return (
+    <Banner title="checkout-ui">
+      {translate('welcome', {target: extension.target})}
+      
+      <Button>Save</Button>
+    </Banner>
+  );
+}
